99ec8d7996935b5fa26de276f84bebe8f79c3439
xiaodai0512 <19339904225@163.com>
2024-10-04 21:19:47 +0800

    Finish

---
+++ build/CMakeFiles/optimizer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/optimizer.dir/main.cpp.o"
+  "CMakeFiles/optimizer.dir/main.cpp.o.d"
+  "optimizer"
+  "optimizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/optimizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ test.cpp
@@ -0,0 +1,64 @@
+//矩阵定义与基本运算
+#include <Eigen/Core>
+//额外的矩阵运算
+#include <Eigen/Dense>
+#include <spdlog/spdlog.h>
+#include "formatter.hpp"
+//Matrix <Scalar, Rows, Cols, Options>
+
+using Eigen::Matrix2d;
+using Eigen::Vector2d;
+
+long id = 2181411945;
+const double step = 0.5;
+const double derta = 0.01;
+const int x = 827;
+const int y = 1709;
+const double d = 1e-8;
+
+double f (Vector2d v){
+    return v.x() * v.x() + v.y() * v.y();
+}
+
+double dv_p (Vector2d v, int pt) {
+    Vector2d vp = v;
+    vp(pt, 0) = (v(pt, 0) + d);
+    return (f(vp) - f(v)) / d;
+}
+
+Matrix2d Hes (Vector2d v){
+    Matrix2d cur;
+    for (int i = 0; i <= 1; i++) {
+        for (int j = 0; j <= 1; j++) {
+            if (i != j) {
+                cur << 0,0,
+                    0,0;
+            }
+            else {
+                cur << 2,0,
+                    0,2;
+            }
+        }
+    }
+    cur = cur.inverse().eval();
+    return cur;
+}
+
+int main(){
+    Vector2d v((id % x) * 1.0, (id % y) * 1.0);
+    while (true) {
+        spdlog::info(v);
+        Vector2d dv(dv_p(v,0), dv_p(v,1)); // 梯度
+        Matrix2d Hessian;
+        Hessian = Hes(v);
+        Vector2d vt = v - (step * (Hessian * dv));
+        if ((vt - v).norm() < derta) {
+            spdlog::info(vt);
+            break;
+        }
+        else {
+            v = vt;
+        }
+    }
+    return 0;
+}
